632f5fbb636dd4d0d2aae1ea19d8cfab8044fcd6
ahmed khan <ahmed.khan@laksol.com>
2017-05-22 11:54:03 +0500

    updating index.php file

--- index.php
+++ index.php
@@ -1,30 +1,15 @@
 <?php
-// ini_set("error_reporting",-1);
 require "bootstrap.php";
 
-// use Illuminate\Database\Capsule\Manager as Capsule;
+$role = Role::Create([
+    "name" => "admnin"
+]);
 
-// Todo::Create(
-//     [
-//         "todo" => "asd",
-//         "category" => "ded",
-//         "description" => "asd"
-//     ]
-// );
-// $passwordkhan = password_hash("ahmedkhan",PASSWORD_DEFAULT);
-// $user = User::where("email","ahmedkhan2@lbs.com")->first();
-// dd(password_verify("ahmedkhan",$user->password));
+$user = User::Create([
+    'name' => "Ahmed Khan",
+    'email' => "ahmed.khan@lbs.com",
+    'password' => password_hash("ahmedkhan",PASSWORD_BCRYPT),
+]);
 
-// $todo = Todo::all()->toArray();
-$roles = Role::find(2);
-// $user = User::Create([
-//     'name' => "Ahmed Khan",
-//     'email' => "ahmed.khan@lbs.com",
-//     'password' => password_hash("ahmedkhan",PASSWORD_BCRYPT),
-// ]);
-$user =User::find(1);
-$permissions =$user->getPermissionsViaRoles();
-foreach ($permissions as $permission) {
-    var_dump($permission->name);
-}
+dd($user->assignRole($role));
 
